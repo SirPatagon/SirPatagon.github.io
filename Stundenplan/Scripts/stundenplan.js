53bb46819bae280e30103928d834ee5fb908538c
Patrick Maihöfer <randommailer66@googlemail.com>
2016-08-09 17:35:01 +0200

    Neue Version hinzugefügt

---
+++ Stundenplan/Scripts/stundenplan.js
@@ -0,0 +1,92 @@
+var ungeradeWoche =
+        '<div data-role="collapsibleset" data-inset="false" data-iconpos="right" data-collapsed-icon="carat-d" data-expanded-icon="carat-u">' +
+            '<div data-role="collapsible">' +
+                '<h3>09:50 - 11:20&emsp;&emsp;IT7+c</h3>' +
+                '<p>' +
+                    'Lehrer: Herr Wichmann' +
+                    '<br />' +
+                    'Raum: 311' +
+                '</p>' +
+            '</div>' +
+            '<div data-role="collapsible">' +
+                '<h3>11:40 - 13:10&emsp;&emsp;IT6+9</h3>' +
+                '<p>' +
+                    'Lehrer: Herr Korte' +
+                    '<br />' +
+                    'Raum: 6' +
+                '</p>' +
+            '</div>' +
+            '<div data-role="collapsible">' +
+                '<h3>13:30 - 15:00&emsp;&emsp;Englisch</h3>' +
+                '<p>' +
+                    'Lehrer: Herr Kriege' +
+                    '<br />' +
+                    'Raum: 4' +
+                '</p>' +
+            '</div>' +
+        '</div>';
+
+var geradeWoche =
+        '<div data-role="collapsibleset" data-inset="false" data-iconpos="right" data-collapsed-icon="carat-d" data-expanded-icon="carat-u">' +
+                '<div data-role="collapsible">' +
+                    '<h3>08:00 - 09:30&emsp;&emsp;IT6+9</h3>' +
+                    '<p>' +
+                        'Lehrer: Herr Korte' +
+                        '<br />' +
+                        'Raum: 324' +
+                    '</p>' +
+                '</div>' +
+                '<div data-role="collapsible">' +
+                    '<h3>09:50 - 11:20&emsp;&emsp;IT6+9</h3>' +
+                    '<p>' +
+                        'Lehrer: Herr Korte' +
+                        '<br />' +
+                        'Raum: 324' +
+                    '</p>' +
+                '</div>' +
+                '<div data-role="collapsible">' +
+                    '<h3>11:40 - 13:10&emsp;&emsp;IT7+c</h3>' +
+                    '<p>' +
+                        'Lehrer: Herr Wichmann' +
+                        '<br />' +
+                        'Raum: 324' +
+                    '</p>' +
+                '</div>' +
+                '<div data-role="collapsible">' +
+                    '<h3>13:30 - 15:00&emsp;&emsp;Englisch</h3>' +
+                    '<p>' +
+                        'Lehrer: Herr Kriege' +
+                        '<br />' +
+                        'Raum: 4' +
+                    '</p>' +
+                '</div>' +
+            '</div>';
+
+function getWeekNumber(d) {
+    // Copy date so don't modify original
+    d = new Date(+d);
+    d.setHours(0, 0, 0);
+    // Set to nearest Thursday: current date + 4 - current day number
+    // Make Sunday's day number 7
+    d.setDate(d.getDate() + 4 - (d.getDay() || 7));
+    // Get first day of year
+    var yearStart = new Date(d.getFullYear(), 0, 1);
+    // Calculate full weeks to nearest Thursday
+    var weekNo = Math.ceil((((d - yearStart) / 86400000) + 1) / 7);
+    // Return array of year and week number
+    return weekNo;
+}
+
+$(function () {
+    "use strict";
+        if (getWeekNumber(new Date()) % 2 == 0) {
+            $("#dieseWoche").html(geradeWoche);
+            $("#naechsteWoche").html(ungeradeWoche);
+        }
+        else {
+            $("#dieseWoche").html(ungeradeWoche);
+            $("#naechsteWoche").html(geradeWoche);
+        }
+
+        $(document).enhanceWithin();
+});
---
+++ Stundenplan/Scripts/vertretung.js
@@ -0,0 +1,25 @@
+function getWeekNumber(d) {
+    // Copy date so don't modify original
+    d = new Date(+d);
+    d.setHours(0, 0, 0);
+    // Set to nearest Thursday: current date + 4 - current day number
+    // Make Sunday's day number 7
+    d.setDate(d.getDate() + 4 - (d.getDay() || 7));
+    // Get first day of year
+    var yearStart = new Date(d.getFullYear(), 0, 1);
+    // Calculate full weeks to nearest Thursday
+    var weekNo = Math.ceil((((d - yearStart) / 86400000) + 1) / 7);
+    // Return array of year and week number
+    return weekNo;
+}
+function GetUrlForWeekNo(nr) {
+    return "http://dev3.bbs-os-brinkstr.de/fileadmin/00info/Stundenplan/1Stdplan_nur_Vertretung/" + nr + "/w/w00037.htm";
+}
+
+$(document).on("pageshow", "#vertretung", function () {
+    "use strict";
+    var weekNo = getWeekNumber(new Date());
+    $('a[href="#one"]').click().focus();
+    $("#vertretungDieseWoche").attr("src", GetUrlForWeekNo(weekNo));
+    $("#vertretungNaechsteWoche").attr("src", GetUrlForWeekNo(weekNo + 1));
+});
