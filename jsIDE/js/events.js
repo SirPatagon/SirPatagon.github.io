63eb839e42ac585ae7c092cc14bae7282b265aae
Patrick Maihöfer <randommailer66@googlemail.com>
2016-12-13 16:36:28 +0100

    Spin Painter angepasst, jsIDE fertiggestellt

--- jsIDE/js/events.js
+++ jsIDE/js/events.js
@@ -9,4 +9,16 @@ $(function () {
     $("#btCloseConsole").on("click", function (e) {
         $("#Console").fadeOut(0);
     });
+
+    $("#aSaveFile").on("click", function (e) {
+        saveFile();
+    });
+
+    $("#aOpenFile").on("click", function (e) {
+        document.getElementById('openFileDlg').click();
+    });
+
+    $("#openFileDlg").on("change", function (e) {
+        openFile(e);
+    });
 });
--- jsIDE/js/init.js
+++ jsIDE/js/init.js
@@ -1,20 +1,56 @@
 var evalCode;
+var saveFile;
+var openFile;
+
+var originalCode;
 
 $(function () {
     //irgendwas ist da kaputt
     var editor = ace.edit("editor");
     editor.setTheme("ace/theme/iplastic");
     editor.getSession().setMode("ace/mode/javascript");
+    originalCode = editor.getValue();
+
+    window.onbeforeunload = function (e) {
+        var test = editor.getValue();
+        if (test != originalCode) {
+            return 'Der Code wurde noch nicht gespeichert! Fortfahren?';
+        }
+    };
+
 
     $("#Console").fadeOut(0);
 
+    saveFile = function () {
+        var a = document.createElement('a');
+        var str = editor.getValue().replace(/\n/g, "\r\n");
+        var file = new Blob([str], { type: 'text/plain' });
+        a.href = URL.createObjectURL(file);
+        a.download = ($("#tbFileName").val() ? $("#tbFileName").val() : "jsIDE-File") + ".js";
+        a.click();
+
+        originalCode = editor.getValue();
+    }
+
+    openFile = function (e) {
+        var file = e.target.files[0];
+        if (!file) {
+            return;
+        }
+        var reader = new FileReader();
+        reader.onload = function (e) {
+            editor.setValue(e.target.result);
+        };
+        reader.readAsText(file);
+    }
+
     function write(str) {
-        var add = str.replace("\n", "<br>");
+        var add = str.replace(/\n/g, "<br>");
         $(".console-core").html($(".console-core").html() + add);
     }
 
     function writeln(str) {
-        var add = str.replace("\n", "<br>");
+        var add = str.replace(/\n/g, "<br>");
         $(".console-core").html($(".console-core").html() + add + "<br>");
     }
 
@@ -29,7 +65,7 @@ $(function () {
         return input;
     }
 
-    evalCode = function() {
+    evalCode = function () {
         $(".console-core").html("");
         try {
             eval("main();\n" + editor.getValue());
