ac88b212d7f7a7291aa76050b2688243ad33c03f
Patrick Maihöfer <randommailer66@googlemail.com>
2017-01-03 16:27:11 +0100

    Automatische Spracherkennung hinzugefügt (Deutsch / Englisch)

---
+++ lang/languages.js
@@ -0,0 +1,33 @@
+var currentLanguage;
+function setRes() {
+    $('*[data-res]').each(function () {
+        $(this).html(currentLanguage[$(this).data("res")]);
+    });
+}
+
+$(function () {
+    var scriptEls = document.getElementsByTagName('script');
+    var thisScriptEl = scriptEls[scriptEls.length - 1];
+    var langFolder = thisScriptEl.src.substr(0, thisScriptEl.src.lastIndexOf('/') + 1);
+
+    var userLang = navigator.language || navigator.userLanguage;
+
+        $.getJSON(langFolder + userLang + ".json", function (result) {
+            currentLanguage = result;
+            setRes();
+        }).fail(function (jqXHR) {
+            if (jqXHR.status == 404) {
+                $.getJSON(langFolder + "en-US.json", function (result) {
+                    currentLanguage = result;
+                    setRes();
+                })
+            } else {
+                alert("Other non-handled error type");
+            }
+        });
+
+        
+
+    
+
+});
